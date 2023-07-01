58fc009bdf43e94fe198ce2e046bb1f3605b633f
LucasT <tressoldi.lucas@gmail.com>
2023-07-01 15:20:10 -0300

    Keyboard dismiss function added

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,6 +5,8 @@ import {
   TextInput,
   TouchableOpacity,
   Vibration,
+  Pressable,
+  Keyboard,
 } from "react-native";
 import ResultImc from "./ResultImc";
 import styles from "./style";
@@ -18,7 +20,8 @@ export default function Form() {
   const [errorMessage, setErrorMessage] = useState(null);
 
   function imcCalculator() {
-    return setImc((weight / (height / 100) ** 2).toFixed(2));
+    let heightFormat = height.replace(",", ".");
+    return setImc((weight / (heightFormat / 100) ** 2).toFixed(2));
   }
 
   function verificationImc() {
@@ -29,6 +32,7 @@ export default function Form() {
   }
 
   function validationImc() {
+    Keyboard.dismiss();
     if (weight != null && height != null) {
       imcCalculator();
       setHeight(null);
@@ -45,7 +49,7 @@ export default function Form() {
   }
 
   return (
-    <View style={styles.formContext}>
+    <Pressable onPress={Keyboard.dismiss} style={styles.formContext}>
       <View style={styles.form}>
         <Text style={styles.formLabel}>Altura</Text>
         <Text style={styles.errorMessage}>{errorMessage}</Text>
@@ -73,6 +77,6 @@ export default function Form() {
         </TouchableOpacity>
       </View>
       <ResultImc messageResultImc={messageImc} resultImc={imc} />
-    </View>
+    </Pressable>
   );
 }
